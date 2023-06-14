9e4cc10a10233fdaa1983acfd229568a89e8a9f5
Fityan <fityannugroho@gmail.com>
2023-06-15 05:45:27 +0700

    feat(component): create Map and its Marker

---
+++ components/Map/Marker/index.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { icon } from 'leaflet'
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png'
+import markerIcon from 'leaflet/dist/images/marker-icon.png'
+import markerShadow from 'leaflet/dist/images/marker-shadow.png'
+import { Marker, MarkerProps, Popup, PopupProps } from 'react-leaflet'
+
+export type MapMarkerProps = MarkerProps & {
+  key?: string
+  PopupProps?: PopupProps
+}
+
+export default function MapMarker({
+  children,
+  PopupProps,
+  ...markerProps
+}: MapMarkerProps) {
+  return (
+    <Marker
+      icon={icon({
+        iconUrl: markerIcon.src,
+        iconRetinaUrl: markerIcon2x.src,
+        shadowUrl: markerShadow.src,
+        iconSize: [24, 40],
+        shadowSize: [0, 0],
+        iconAnchor: [12, 40],
+        popupAnchor: [0, -40],
+      })}
+      {...markerProps}
+    >
+      {children && (
+        <Popup {...PopupProps}>{children}</Popup>
+      )}
+    </Marker>
+  )
+}
--- components/Map/index.tsx
+++ components/Map/index.tsx
@@ -1,29 +1,37 @@
 'use client'
 
-import {
-  MapContainer, TileLayer, MapContainerProps,
-} from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import { MapContainer, MapContainerProps, TileLayer } from 'react-leaflet'
+import MapMarker, { MapMarkerProps } from './Marker'
 
-export type MapProps = MapContainerProps
+export type MapProps = MapContainerProps & {
+  markers?: MapMarkerProps[]
+}
 
 export default function Map({
   center = [-3.028137, 119.764063],
   className = 'w-full h-96',
-  children,
+  markers = [],
   zoom = 5,
+  ...mapProps
 }: MapProps) {
   return (
     <MapContainer
       center={center}
       zoom={zoom}
       className={className}
+      {...mapProps}
     >
       <TileLayer
         attribution='<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {children}
+
+      {markers.map(({ children, key, ...marker }) => (
+        <MapMarker key={key} {...marker}>
+          {children}
+        </MapMarker>
+      ))}
     </MapContainer>
   )
 }
