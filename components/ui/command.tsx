48c6fa8a82fed1df8de4ad5a3006adf53446abb3
Fityan <fityannugroho@gmail.com>
2024-02-12 06:40:47 +0700

    fix: value escaping in `CommandItem` component (#10)
    Revert this when there is a patch update from `cmdk`

--- components/ui/command.tsx
+++ components/ui/command.tsx
@@ -113,13 +113,14 @@ CommandSeparator.displayName = CommandPrimitive.Separator.displayName
 const CommandItem = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Item>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Item>
->(({ className, ...props }, ref) => (
+>(({ className, value, ...props }, ref) => (
   <CommandPrimitive.Item
     ref={ref}
     className={cn(
       'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm outline-none aria-selected:bg-accent aria-selected:text-accent-foreground data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
       className,
     )}
+    value={value?.replaceAll('"', '\\"')}
     {...props}
   />
 ))
