4f3788cdd0b16fd770273068cba6b23fdbc45594
Fityan <fityannugroho@gmail.com>
2024-02-21 20:12:16 +0700

    refactor: remove area-boundary api endpoint, use server action instead

--- lib/utils.ts
+++ lib/utils.ts
@@ -32,3 +32,21 @@ export function debounce<T extends any[]>(
 export function ucFirstStr(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+/**
+ * Add dot separator to the code.
+ * - 4 digits (e.g. 9603) becomes 5 digits (e.g. 96.03)
+ * - 6 digits (e.g. 960301) becomes 8 digits (e.g. 96.03.01)
+ * - 10 digits (e.g. 9603011001) becomes 12 digits (e.g. 96.03.01.1001)
+ */
+export function addDotSeparator(code: string) {
+  const codeLength = code.length
+
+  if (codeLength === 4) return `${code.slice(0, 2)}.${code.slice(2)}`
+  if (codeLength === 6)
+    return `${code.slice(0, 2)}.${code.slice(2, 4)}.${code.slice(4)}`
+  if (codeLength === 10)
+    return `${code.slice(0, 2)}.${code.slice(2, 4)}.${code.slice(4, 6)}.${code.slice(6)}`
+
+  return code
+}
