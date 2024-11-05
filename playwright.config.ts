841bb400529f9ab1dc9a218028faf8175e57f517
Fityan <63894003+fityannugroho@users.noreply.github.com>
2024-11-05 18:34:54 +0700

    chore: update CI workflow to build project and configure Playwright web server settings

--- playwright.config.ts
+++ playwright.config.ts
@@ -74,9 +74,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: config.appUrl,
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000, // 120 seconds
+  },
 })
