9912208abd16055ad58669e0a325a807163a90c8
Fityan <63894003+fityannugroho@users.noreply.github.com>
2024-11-10 20:20:51 +0700

    chore: update Playwright config to use pnpm for starting the local dev server

--- playwright.config.ts
+++ playwright.config.ts
@@ -75,7 +75,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
+    command: 'pnpm run start',
     url: config.appUrl,
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000, // 120 seconds
